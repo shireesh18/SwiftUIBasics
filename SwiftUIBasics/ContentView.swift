b19690147d15a73c41c0fad725bd9cc4f5c75d25
Shireesh Marla <shireesh.marla@swipiicard.com>
2021-12-02 11:14:14 +0000

    Initial Commit

---
+++ SwiftUIBasics/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIBasics
+//
+//  Created by Shireesh Marla on 02/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIBasics/SwiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicsApp.swift
+//  SwiftUIBasics
+//
+//  Created by Shireesh Marla on 02/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
